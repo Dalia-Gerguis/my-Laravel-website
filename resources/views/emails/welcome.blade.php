28454ac4e19633eec72ccfb2fdac8915726003fa
dalia25 <dodo.gerges22@gmail.com>
2019-07-07 20:24:21 +0200

    Added REST Api

---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h2>Learning Laravel!</h2>
+        <div>
+            welcome to {!! $name !!} website!
+        </div>
+    </body>
+</html>
